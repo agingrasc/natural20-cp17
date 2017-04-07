64bd9969bde2e80d6f4d2914d8568d371e50a07a
Philippe Babin <philippe.babin@gmail.com>
2017-04-07 03:44:22 -0400

    Add dialog action handling in DayState

--- src/domain/state/daystate.py
+++ src/domain/state/daystate.py
@@ -1,4 +1,5 @@
 from domain.state.state import State
+from display.action.dialog import Dialog
 
 class DayState(State):
     def __init__(self, day):
@@ -7,13 +8,28 @@ class DayState(State):
 
     def introduce_next_client(self, dt, action):
         self.current_client = self.day.pop_clients()
-        self.change_substate_and_exec(self.move_client_to_elevator)
+        return self.change_substate_and_exec(self.move_client_to_elevator)
 
     def move_client_to_elevator(self, dt, actions):
+        # Should wait for end of animate
+        self.next_substate = self.init_dialog
+
+    def init_dialog(self, dt, actions):
+        self.current_dialog = Dialog(self.current_client.greet())
+        self.next_substate = self.wait_for_end_dialog
+        return self.current_dialog
+
+    def wait_for_end_dialog(self, dt, actions):
+        if isinstance(actions, DialogOver):
+            self.next_substate = self.init_player_input
+        else:
+            return self.current_dialog
+
+    def init_player_input(self, dt, actions):
         pass
 
     def exec(self, dt=None, actions=None):
-        self.current_substate(dt, actions)
+        return self.current_substate(dt, actions)
 
     def is_finish(self):
         pass
